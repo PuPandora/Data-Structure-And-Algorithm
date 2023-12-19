777877c50616d2af8f30e55b0b44d9a755a9c932
PuPandora <pandora6722@gmail.com>
2023-12-19 17:49:19 +0900

    Add: 동적 배열 (List)
    + C#의 동적 배열 List를 직접 구현
    + Add, Remove, RemoveAt 추가와 제거 함수
    + Find, FindAll 탐색 함수 (선형)

---
+++ DataStructure/List.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pandora
+{
+    internal class List<T>
+    {
+        private T[] items;
+        private int size = 0;
+
+        public int count { get => size; }
+
+        public int Capacity
+        {
+            get => items.Length;
+            set
+            {
+                if (value != items.Length)
+                {
+                    // 1이상 설정할 경우
+                    if (value > 0)
+                    {
+                        T[] newItems = new T[value];
+
+                        // 이미 리스트에 요소가 있다면 복사
+                        if (size > 0)
+                        {
+                            for (int i = 0; i < size; i++)
+                            {
+                                newItems[i] = items[i];
+                            }
+                        }
+                        items = newItems;
+                    }
+                    else
+                    {
+                        items = new T[0];
+                    }
+                }
+            }
+        }
+        private const int defaultCapacity = 4;
+
+        public List()
+        {
+            items = new T[0];
+            size = 0;
+        }
+
+        public T this[int index]
+        {
+            get => items[index];
+            set
+            {
+                if (index >= items.Length)
+                {
+                    items[index] = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 리스트에 요소를 추가하는 함수
+        /// </summary>
+        public void Add(T item)
+        {
+            if (size == items.Length)
+            {
+                EnsureCapacity();
+            }
+
+            items[size] = item;
+            size++;
+        }
+
+        /// <summary>
+        /// 리스트에서 가장 먼저 탐색되는 특정 요소를 제거하는 함수
+        /// </summary>
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(items, item, 0, size);
+        }
+
+        /// <summary>
+        /// 리스트에서 특정 인덱스에 있는 요소를 제거하는 함수
+        /// </summary>
+        public void RemoveAt(int index)
+        {
+            if (index < size)
+            {
+                size--;
+                Array.Copy(items, index + 1, items, index, size - index);
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            items[size] = default(T);
+        }
+
+        /// <summary>
+        /// 리스트에서 가장 먼저 match 조건에 맞는 요소를 반환하는 함수
+        /// </summary>
+        public T Find(Predicate<T> match)
+        {
+            foreach (T item in items)
+            {
+                if (match(item)) return item;
+            }
+
+            return default(T);
+        }
+
+        /// <summary>
+        /// 리스트에서 match 조건에 맞는 모든 요소를 담은 리스트를 반환하는 함수
+        /// </summary>
+        public List<T> FindAll(Predicate<T> match)
+        {
+            List<T> result = new List<T>();
+
+            foreach (T item in items)
+            {
+                if (match(item)) result.Add(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 리스트의 Capacity를 보장하는 함수
+        /// <br></br>
+        /// 배열의 길이가 0이라면 DefaultCapacity인 4로 설정
+        /// <br></br>
+        /// 0보다 크다면 배열 길이의 2배로 Capacity를 설정
+        /// </summary>
+        private void EnsureCapacity()
+        {
+            int doubleCapacity = items.Length == 0 ? defaultCapacity : items.Length * 2;
+            Capacity = doubleCapacity;
+        }
+
+        // IEnumerator, Foreach 사용
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                yield return items[i];
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -10,6 +10,47 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
+            Pandora.List<int> listB = new Pandora.List<int>();
+
+            // 리스트 추가, 용량 테스트
+            Console.WriteLine("=== 리스트 추가, 용량 테스트 ===");
+            for (int i = 0; i < 25;  i++)
+            {
+                listB.Add(i);
+                Console.WriteLine($"List[{i}] : {listB[i]}");
+                Console.WriteLine($"List Capacity : {listB.Capacity}");
+                Console.WriteLine();
+            }
+
+            // 리스트 요소 제거 테스트
+            Console.WriteLine("\n=== 리스트 요소 제거 테스트 ===");
+            Console.WriteLine("Remove(5)");
+            listB.Remove(5);
+            foreach (var item in listB)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("RemoveAt(1)");
+            listB.RemoveAt(1);
+            foreach (var item in listB)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
+            // 리스트 탐색 테스트
+            Console.WriteLine("\n=== 리스트 요소 탐색 테스트 ===");
+            Console.WriteLine($"Find(4) : {listB.Find((a) => a == 4)}");
+            Console.WriteLine($"Find(5) : {listB.Find((a) => a == 5)}");
+
+            Console.WriteLine($"FindAll((a) => a <= 6) (해당 리스트의 6이하 요소들만 찾기");
+            var tmpList = listB.FindAll((a) => a <= 6);
+            for (int i = 0; i < tmpList.count; i++)
+            {
+                Console.Write($"{tmpList[i]}, ");
+            }
         }
     }
 }
