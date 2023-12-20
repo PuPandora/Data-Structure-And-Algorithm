f8c6a418ed257764eba664d80948d7832903d54e
PuPandora <pandora6722@gmail.com>
2023-12-20 17:54:13 +0900

    Add: 연결 리스트 (Linked List)
    C#의 Linked List 구현
    Linked List : 노드형 선형 자료구조
    
    + AddFirst, AddLast 추가 함수
    + Find, FindLast 탐색 함수
    + RemoveFirst, RemoveLast, Remove, Clear 제거 함수

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandora
+{
+    public class LinkedListNode<T>
+    {
+        public T data;
+        public LinkedListNode<T> next;
+        public LinkedListNode<T> prev;
+
+        public LinkedListNode(T data)
+        {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        /// <summary>
+        /// LinkedList의 첫 번째 노드
+        /// </summary>
+        public LinkedListNode<T> first { get; private set; }
+
+        /// <summary>
+        /// LinkedList의 마지막 노드
+        /// </summary>
+        public LinkedListNode<T> last { get; private set; }
+
+        /// <summary>
+        /// LinkedList가 가진 모든 노드의 수
+        /// </summary>
+        public int count { get; private set; }
+
+        /// <summary>
+        /// 앞에서부터 노드를 추가하는 함수
+        /// </summary>
+        public void AddFirst(T data)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(data);
+
+            if (first == null)
+            {
+                first = newNode;
+                last = newNode;
+            }
+            else
+            {
+                newNode.next = first;
+                first.prev = newNode;
+                first = newNode;
+            }
+
+            count++;
+        }
+
+        /// <summary>
+        /// 뒤에서부터 노드를 추가하는 함수
+        /// </summary>
+        public void AddLast(T data)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(data);
+
+            if (first == null)
+            {
+                first = newNode;
+                last = newNode;
+            }
+            else
+            {
+                last.next = newNode;
+                newNode.prev = last;
+
+                last = newNode;
+            }
+
+            count++;
+        }
+
+        /// <summary>
+        /// 앞에서부터 데이터와 동일한 값을 가진 첫 번째 노드를 찾는 함수
+        /// </summary>
+        public LinkedListNode<T> Find(T data)
+        {
+            var node = first;
+
+            while (node.next != null)
+            {
+                if (node.data.Equals(data))
+                {
+                    return node;
+                }
+
+                node = node.next;
+            }
+
+            return default(LinkedListNode<T>);
+        }
+
+        /// <summary>
+        /// 뒤에서부터 데이터와 동일한 값을 가진 첫 번째 노드를 찾는 함수
+        /// </summary>
+        public LinkedListNode<T> FindLast(T data)
+        {
+            var node = last;
+
+            while (last.prev != null)
+            {
+                if (node.data.Equals(data))
+                {
+                    return node;
+                }
+
+                node = node.prev;
+            }
+
+            return default(LinkedListNode<T>);
+        }
+
+        /// <summary>
+        /// 첫번째 노드를 제거하는 함수
+        /// </summary>
+        public void RemoveFirst()
+        {
+            first = first.next;
+            first.prev = null;
+            count--;
+        }
+
+        /// <summary>
+        /// 마지막 노드를 제거하는 함수
+        /// </summary>
+        public void RemoveLast()
+        {
+            last = last.prev;
+            last.next = null;
+            count--;
+        }
+
+        /// <summary>
+        /// 데이터와 동일한 값을 가진 노드를 제거하는 함수
+        /// </summary>
+        public bool Remove(T data)
+        {
+            var node = Find(data);
+            if (node != null)
+            {
+                node.next.prev = node.prev;
+                node.prev.next = node.next;
+                count--;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 모든 노드를 제거하는 함수
+        /// </summary>
+        public void Clear()
+        {
+            first = null;
+            last = null;
+            count = 0;
+        }
+
+        // Foreach
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = first;
+            while (node != null)
+            {
+                yield return node.data;
+                node = node.next;
+            }
+        }
+
+        /// <summary>
+        /// 연결 리스트의 모든 요소를 출력하는 함수
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"노드의 개수 : {count}");
+
+            var node = first;
+            while (node != null)
+            {
+                Console.Write($"{node.data}");
+                if (node.next != null)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+
+                node = node.next;
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -10,47 +10,58 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Pandora.List<int> listB = new Pandora.List<int>();
+            Pandora.LinkedList<int> linkedList = new Pandora.LinkedList<int>();
 
-            // 리스트 추가, 용량 테스트
-            Console.WriteLine("=== 리스트 추가, 용량 테스트 ===");
-            for (int i = 0; i < 25;  i++)
-            {
-                listB.Add(i);
-                Console.WriteLine($"List[{i}] : {listB[i]}");
-                Console.WriteLine($"List Capacity : {listB.Capacity}");
-                Console.WriteLine();
-            }
-
-            // 리스트 요소 제거 테스트
-            Console.WriteLine("\n=== 리스트 요소 제거 테스트 ===");
-            Console.WriteLine("Remove(5)");
-            listB.Remove(5);
-            foreach (var item in listB)
-            {
-                Console.Write($"{item}, ");
-            }
+            // 추가 테스트
+            Console.WriteLine("=== AddFirst 테스트 ===");
+            linkedList.AddFirst(1);
+            linkedList.AddFirst(2);
+            linkedList.AddFirst(3);
+            linkedList.AddFirst(4);
+            linkedList.AddFirst(5);
+            linkedList.Print();
             Console.WriteLine();
 
-            Console.WriteLine("RemoveAt(1)");
-            listB.RemoveAt(1);
-            foreach (var item in listB)
-            {
-                Console.Write($"{item}, ");
-            }
+            // 추가 테스트 2
+            Console.WriteLine("=== AddLast 테스트 ===");
+            linkedList.AddLast(11);
+            linkedList.AddLast(12);
+            linkedList.AddLast(13);
+            linkedList.AddLast(14);
+            linkedList.AddLast(15);
+            linkedList.Print();
             Console.WriteLine();
 
-            // 리스트 탐색 테스트
-            Console.WriteLine("\n=== 리스트 요소 탐색 테스트 ===");
-            Console.WriteLine($"Find(4) : {listB.Find((a) => a == 4)}");
-            Console.WriteLine($"Find(5) : {listB.Find((a) => a == 5)}");
+            // 탐색 테스트
+            Console.WriteLine("=== Find 테스트 ===");
+            Console.WriteLine($"linkedList.Find(4).data : {linkedList.Find(4).data}");
+            Console.WriteLine($"linkedList.Find(20) : {linkedList.Find(20)}");
+            Console.WriteLine();
 
-            Console.WriteLine($"FindAll((a) => a <= 6) (해당 리스트의 6이하 요소들만 찾기");
-            var tmpList = listB.FindAll((a) => a <= 6);
-            for (int i = 0; i < tmpList.count; i++)
+            // 제거 테스트
+            Console.WriteLine("=== RemoveFirst 테스트 ===");
+            Console.WriteLine("RemoveFirst * 3");
+            for (int i = 0; i < 3; i++)
             {
-                Console.Write($"{tmpList[i]}, ");
+                linkedList.RemoveFirst();
             }
+            linkedList.Print();
+            Console.WriteLine();
+
+            // 제거 테스트 2
+            Console.WriteLine("=== RemoveLast 테스트 ===");
+            Console.WriteLine("RemoveLast * 3");
+            for (int i = 0; i < 3; i++)
+            {
+                linkedList.RemoveLast();
+            }
+            linkedList.Print();
+
+            // 제거 테스트 3
+            Console.WriteLine("=== Clear 테스트 ===");
+            linkedList.Clear();
+            linkedList.Print();
+            Console.WriteLine();
         }
     }
 }
